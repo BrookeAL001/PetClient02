806357bfb3d67f915f98269470c56938db574e56
BrookeAL0012 <bbcecil17@gmail.com>
2023-01-02 14:00:40 -0600

    Add project files.

---
+++ PetClient02/Controllers/ClientsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PetClient02.Data;
+
+namespace PetClient02.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ClientsController : Controller
+    {
+        private readonly ClientsApiDbContext dbContext;
+        public ClientsController(ClientsApiDbContext dbContext)
+        {
+
+        }
+        [HttpGet]
+        public IActionResult GetClients()
+        {
+           return Ok(dbContext.Clients.ToList());
+            
+        }
+    }
+}
---
+++ PetClient02/Data/ClientsApiDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PetClient02.Models;
+
+namespace PetClient02.Data
+{
+    public class ClientsApiDbContext : DbContext
+    {
+        public ClientsApiDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Client> Clients { get; set; }
+    }
+}
